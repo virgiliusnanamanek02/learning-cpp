1b640a4f1612650e7f31930f3bda02112bd65d49
Vicky Nana <virgilius.nana.manek0719@gmail.com>
2023-02-23 02:46:05 +0700

    user input

---
+++ userinput.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    int age;
+    cout << "Hello, World!" << endl;
+    cout << "What is your name?" << endl;
+    cin >> name;
+    cout << "Hello, this is " << name << "!" << endl;
+    cout << "How old are you?" << endl;
+    cin >> age;
+    cout << "I am " << age << " years old." << endl;
+    return 0;
+}
