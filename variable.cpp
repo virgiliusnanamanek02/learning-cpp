cbd49c330b3e567dbb1a54ff3e4cd587ac96cc74
Vicky Nana <virgilius.nana.manek0719@gmail.com>
2023-02-23 02:45:31 +0700

    Variable

---
+++ variable.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    string name = "Vicky";
+    int age = 20;
+    cout << "Hello, World!" << endl;
+    cout << "Hello, this is " << name << "!" << endl;
+    cout << "I am " << age << " years old." << endl;
+}
